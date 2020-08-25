a01f9ae38a67d3bc53dc99854dea8e920fc2189b
daniu <dbrisaro@gmail.com>
2020-08-25 14:22:35 -0300

    inicio repo del hub

---
+++ comandos-utiles.sh
@@ -0,0 +1,14 @@
+# lista inside helm
+helm ls
+
+# upgrade 
+helm upgrade jhub jupyterhub/jupyterhub --version=v0.9.0 --values config.yaml
+
+#You can find if the hub and proxy is ready by doing:
+
+kubectl --namespace=jhub get pod
+
+#and watching for both those pods to be in status 'Running'.
+#You can find the public IP of the JupyterHub by doing:
+
+kubectl --namespace=jhub get svc proxy-public
